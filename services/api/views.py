5f02f99752c1e29ef46c0b3da28aa25b4294c581
Steve Anderson <steveanderson@hotmail.ca>
2016-01-20 13:55:03 +1000

    Update to the view for json

--- services/api/views.py
+++ services/api/views.py
@@ -1,5 +1,12 @@
+import json
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def index(request):
-    return HttpResponse("Testing this all out")
+    data = {
+        "title":"Some tree",
+        "answers": [
+            {"title":"some answer"}
+        ]
+    }
+    return JsonResponse(data)
