0ca929486eb956b4fc6c47d6fc18d9a955017f5a
Steve Anderson <steveanderson@hotmail.ca>
2016-02-02 14:55:05 +1000

    Added for models

--- services/test.py
+++ services/test.py
@@ -9,7 +9,7 @@ mc = UserMongoContext('localhost', 27017)
 sc = SessionService(mc)
 h = Handler(c, sc)
 
-result = h.getById(115, 1)
+result = h.getById(121, 1, True)
 
 #result = h.getById(113, 1)
 
