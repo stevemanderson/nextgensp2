03dc3952a8711068ecf7258936dc39233173dca9
Steve Anderson <steveanderson@hotmail.ca>
2016-04-05 10:05:09 +1000

    update for services

--- services/services/settings.py
+++ services/services/settings.py
@@ -97,9 +97,17 @@ WSGI_APPLICATION = 'services.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nextgensp2',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
